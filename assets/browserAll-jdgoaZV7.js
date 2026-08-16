d8486c78b9c5973c4ead6f0d6c7558555edb0c06
mezzo-mazzo <143316523+mezzo-mazzo@users.noreply.github.com>
2026-08-16 21:05:26 +0300

    v0.2.0

---
+++ assets/browserAll-jdgoaZV7.js
@@ -0,0 +1,1 @@
+import{e as a,A as e,C as i,a as s,D as t,E as n,F as d}from"./pixi-DEDNIltA.js";import"./webworkerAll-BjQANylI.js";a.add(e);a.mixin(i,s);a.add(t);a.add(n);a.mixin(i,d);
---
+++ assets/webworkerAll-BjQANylI.js
@@ -0,0 +1,1 @@
+import{e,j as s,k as a,l as t}from"./pixi-DEDNIltA.js";e.add(s,a);e.add(t);
